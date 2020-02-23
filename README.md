cecabb25e0da60261c4bd45cf9b90d647144c31e
VladKonko <58661322+VladKonko@users.noreply.github.com>
2020-02-23 18:31:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# superScheduler-mob
+ 
---
+++ src/test/java/com/telran/scheduler/FirstTest.java
@@ -0,0 +1,39 @@
+package com.telran.scheduler;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FirstTest {
+    AppiumDriver driver;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName","Android");
+        caps.setCapability("deviceName","QA22");
+        caps.setCapability("platformVersion","8.0");
+        caps.setCapability("automationName","Appium");
+        caps.setCapability("appPackage","com.example.svetlana.scheduler");
+        caps.setCapability("appActivity",".presentation.splashScreen.SplashScreenActivity");
+        caps.setCapability("app","C:/Users/VladQA/Documents/GitHub/superScheduler-mob/src/test/resources/apk/v.0.0.3.apk");
+    driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+
+
+    }
+    @Test
+    public void testOpenApp(){
+        System.out.println("gooo");
+    }
+
+    @AfterClass
+    public void tearDown() throws InterruptedException {
+    Thread.sleep(4000);
+    driver.quit();
+    }
+}
